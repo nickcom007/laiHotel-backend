b117af52a662ae102eab89e9906fb4049446ab59
Nick Wen <nickcom007@gmail.com>
2022-01-02 22:08:02 +0000

    add ignore

--- app/model/database.py
+++ app/model/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "mariadb+pymysql://admin:nonprodpassword@database-1.crm9o9nyoccr.us-east-2.rds.amazonaws.com/laihotel"
+load_dotenv()
+
+DB_URL = os.environ['DB_URL']
+SQLALCHEMY_DATABASE_URL = f"mariadb+pymysql://{DB_URL}"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
 engine = create_engine(
